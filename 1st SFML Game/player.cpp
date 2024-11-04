39bb382e87939dc843db75b012221fb6a8c96d8f
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2024-11-03 22:51:36 -0800

    fixed problem with moving to target when mouse position is below the player and added some comments

--- 1st SFML Game/player.cpp
+++ 1st SFML Game/player.cpp
@@ -5,20 +5,20 @@ void Player::update(RenderWindow& wn)
 {
 	if (Mouse::isButtonPressed(Mouse::Right))
 	{
-		this->target_pos = new Vector2i(Mouse::getPosition(wn));
+		this->target_pos = new Vector2i(Mouse::getPosition(wn)); //Allocate a new Vector2i pointer pointing to a mouse position
 	}
 
 	if (this->target_pos)
 	{
-		Vector2f curr = this->getRect().getPosition();
-		bool reach_dest = (curr.x - this->target_pos->x < 1) && (curr.y - this->target_pos->y < 1);
+		Vector2f curr = this->getRect().getPosition(); //The curr player postion
+		bool reach_dest = (abs(curr.x - this->target_pos->x) < 3) && (abs(curr.y - this->target_pos->y) < 3); //Check the player and the target pos is within 3 pixel of each other
 
-		if (!reach_dest)
+		if (!reach_dest) //Keep moving if it hasn't reach its destination
 		{
 		this->move(*(this->target_pos));
 		}
 
-		else
+		else //if it has set it to a nullptr so that it doesn't try to keep moving
 		{
 			this->target_pos = nullptr;
 		}
