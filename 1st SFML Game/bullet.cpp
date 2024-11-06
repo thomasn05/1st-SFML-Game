53bacf66aec6054acf67287b92547cf736bc72b7
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2024-11-06 14:09:23 -0800

    spawn bullet infront of player

--- 1st SFML Game/bullet.cpp
+++ 1st SFML Game/bullet.cpp
@@ -1,7 +1,7 @@
 #include "bullet.h"
 #include <iostream>
 
-Bullet::Bullet(Entity host, float angle) : Entity(host.getRect().getPosition(), Vector2f(5, 10), Color::Red)
+Bullet::Bullet(Entity host, float angle) : Entity(this->getRect().getPosition(), this->size, Color::Red)
 {
 	FloatRect host_bound = host.getRect().getGlobalBounds();
 	float x_dist = ((host_bound.getSize().x) / 2.f) + (this->size.x / 2.f); //How far from the host distance the bullet need to spawn
@@ -9,6 +9,7 @@ Bullet::Bullet(Entity host, float angle) : Entity(host.getRect().getPosition(), 
 	Vector2f pos = host_bound.getPosition();
 
 	Vector2f spawn = Vector2f(pos.x - angle * x_dist, pos.y - angle * max_distance);
+	this->getRect().setPosition(spawn);
 }
 
 void Bullet::update(RenderWindow& wn)
