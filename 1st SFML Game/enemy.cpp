bb0b27d713b5c57ccc34871cfa1a5e3a73f6f5a9
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2024-11-10 01:24:45 -0800

    enemy now takes a player instead of an enetity

--- 1st SFML Game/enemy.cpp
+++ 1st SFML Game/enemy.cpp
@@ -1,10 +1,9 @@
 #include "enemy.h"
 #include <random>
-#include <iostream>
 
-float coord(float coord, int range)
+int coord(float coord, int range)
 {
-	float pos;
+	int pos;
 
 	while (true)
 	{
@@ -20,7 +19,7 @@ float coord(float coord, int range)
 	return pos;
 }
 
-Vector2f spawn(Entity player)
+Vector2f spawn(Player player)
 {
 	float x_pos = coord(player.object.getPosition().x, 1400);
 	float y_pos = coord(player.object.getPosition().y, 1000);
@@ -32,4 +31,19 @@ void Enemy::update(RenderWindow& wn)
 {
 	this->move((Vector2i)(player.object.getPosition()), this->speed);
 	wn.draw(this->object);
+}
+
+bool Enemy::is_dead()
+{
+	std::vector<Bullet> player_bullets = this->player.get_bullets();
+	for (auto b = player_bullets.begin(); b !=player_bullets.end();)
+	{
+		if (this->collide(*b))
+		{
+			this->lifespan = seconds(0);
+			break;
+		}
+	}
+
+	return this->lifespan == seconds(0);
 }
--- 1st SFML Game/enemy.h
+++ 1st SFML Game/enemy.h
@@ -1,24 +1,26 @@
 #pragma once
 #include "entity.h"
+#include "player.h"
 
 const float spawn_dist = 200;
 const Vector2f size = Vector2f(25, 25);
 const int ENEMY_SPEED = 4;
 
-float coord(float coord, int range);
+int coord(float coord, int range);
 
-Vector2f spawn(Entity player);
+Vector2f spawn(Player player);
 
 class Enemy :
     public Entity
 {
 private:
-    Entity &player;
+    Player& player;
     int speed = ENEMY_SPEED;
 
 public:
-    Enemy(Entity &player) : Entity(spawn(player), size, Color::Red), player(player) {}
+    Enemy(Player& player) : Entity(spawn(player), size, Color::Red), player(player) {}
 
     void update(RenderWindow& wn);
-};
 
+    bool is_dead();
+};
