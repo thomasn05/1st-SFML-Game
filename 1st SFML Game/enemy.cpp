07fe75f3d670faa188aaf89954e5b1ac5f220d89
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2024-11-12 00:44:18 -0800

    enemy docstring

--- 1st SFML Game/enemy.cpp
+++ 1st SFML Game/enemy.cpp
@@ -2,13 +2,13 @@
 #include <random>
 #include <iostream>
 
-int coord(float coord, int range)
+int coord(float coord, int range) //Generate a pos in range away from coord
 {
 	int pos;
 
-	while (true)
+	while (true) //Keep generating a new pos if it has within spawn_dist of coord
 	{
-		std::random_device rd;
+		std::random_device rd; //Random generator
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<> distrib(25, range - 25);
 
@@ -20,25 +20,25 @@ int coord(float coord, int range)
 	return pos;
 }
 
-Vector2f spawn(Player player)
+Vector2f spawn(Player player) //Create a position Vector around the player
 {
-	float x_pos = (float)coord(player.object.getPosition().x, 1400);
+	float x_pos = (float)coord(player.object.getPosition().x, 1400); //Create x and y coord
 	float y_pos = (float)coord(player.object.getPosition().y, 1000);
 
 	return Vector2f(x_pos, y_pos);
 }
 
-void Enemy::update(RenderWindow& wn, Player player)
+void Enemy::update(RenderWindow& wn, Player player) //Move enemy towards player
 {
 	this->move((Vector2i)player.object.getPosition(), this->speed);
 	wn.draw(this->object);
 }
 
-bool Enemy::is_dead(std::vector<Bullet>& player_bullets)
+bool Enemy::is_dead(std::vector<Bullet>& player_bullets) //Check if enemy has collide with player's bullets
 {
 	for (auto b = player_bullets.begin(); b !=player_bullets.end();)
 	{
-		if (this->collide(*b))
+		if (this->collide(*b)) //If collide remove the bullet from player's bullets and kill this enemy
 		{
 			player_bullets.erase(b);
 			this->lifespan = seconds(0);
--- 1st SFML Game/enemy.h
+++ 1st SFML Game/enemy.h
@@ -6,8 +6,19 @@ const float spawn_dist = 200;
 const Vector2f size = Vector2f(25, 25);
 const int ENEMY_SPEED = 4;
 
+/*
+* @brief Randomizes a coordinate between a certain range at a certain distance away from coord
+* @param coord: a coord that the new coord will generate away from
+* @param range: the range of the new cord
+* @return a new coord
+*/
 int coord(float coord, int range);
 
+/*
+* @brief Create a spawning vector around the player
+* @param player: a Player object to generate a new position around
+* @return a Vector position
+*/
 Vector2f spawn(Player player);
 
 class Enemy :
@@ -17,9 +28,21 @@ private:
     int speed = ENEMY_SPEED;
 
 public:
+    /*
+    *@brief Create an Enemy Entity at a given spawn
+    * @param spawn: thhe spawning location of Enenmy
+    */
     Enemy(Vector2f spawn) : Entity(spawn, size, Color::Red) { this->lifespan = seconds(1); }
 
+    /*
+    * @brief update the Enemy object to move towards the player
+    */
     void update(RenderWindow& wn, Player player);
 
+    /*
+    * @brief check if the Enemy object has collide with a player bullet. If it has collide it will delete the bullet from the vector and kill the Enemy.
+    * @param player_bullets: a reference of the Player's bullet
+    * @return True if the Enemy has collide with a bullet
+    */
     bool is_dead(std::vector<Bullet>& player_bullets);
 };
