f55775f4e5df7fe8ba201753b5fef5d6d9214bdd
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2024-11-19 20:59:31 -0800

    SAT Collision between two Entity object

--- 1st SFML Game/src/main.cpp
+++ 1st SFML Game/src/main.cpp
@@ -41,7 +41,9 @@ int main()
     if (!font.loadFromFile("arial.ttf")) { return -1; }
 
     Player player(player_spawn);
-    EnemyManager E_manager(player);
+    Entity object(Vector2f(100, 100), Vector2f(50, 50), Color::Blue);
+    
+    //EnemyManager E_manager(player);
 
     while (window.isOpen())
     {
@@ -59,9 +61,17 @@ int main()
         if (!player.is_dead())
         {
             player.update(window, game_timer.getElapsedTime());
+            player.object.setFillColor(Color::Green);
             //E_manager.update(window, game_timer.getElapsedTime());
+
+            if (SAT_Collision(player.get_corners(), object.get_corners()))
+            {
+                player.object.setFillColor(Color::Red);
+            }
         }
         else { draw_end_screen(font, window); }
+
+        window.draw(object.object);
 
         window.display();
     }
