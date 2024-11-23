a6ceda92eadcb4345f1b98136005696c1f73f1b1
Thomas Nguyen <Tuancalifornia714@yahoo.com>
2024-11-22 22:10:48 -0800

    delete draw_end and draw_start and combine them into one function that draw a given text on screen

--- 1st SFML Game/inc/game_engine.h
+++ 1st SFML Game/inc/game_engine.h
@@ -9,15 +9,15 @@ const int win_height = 1000;
 const Vector2f player_spawn = Vector2f(win_width / 2, win_height / 2); // @brief location of player starting position
 
 const int end_game_font_size = 100; // @brief size of end_game_text
-const Vector2f end_text_spawn = Vector2f(win_width / 2 - end_game_font_size / 2 - 225, win_height / 2 - end_game_font_size / 2); // @brief the pos of the "Game Over!" text
+const Vector2f end_text_spawn = Vector2f(win_width / 2, win_height / 2 - 50); // @brief the pos of the "Game Over!" text
 
 const int score_font_size = 50;
-const Vector2f score_pos = Vector2f(100, 75);
+const Vector2f score_pos = Vector2f(150, 75);
 
 const int start_font_size = 100;
-const Vector2f start_text_pos = Vector2f(win_width / 2 - start_font_size / 2 - 75, win_height / 2 - start_font_size / 2 - 75);
+const Vector2f start_text_pos = Vector2f(win_width / 2, win_height / 2 - 75);
 
-Text get_text(const int font_size, Color color, Vector2f pos);
+Text get_text(const int font_size, Color color);
 
 class Game_engine
 {
@@ -27,18 +27,16 @@ private:
 	Font font;
 	Player& player = player;
 	EnemyManager& e_manager;
-	Text end_text = get_text(end_game_font_size, Color::Red, end_text_spawn);
-	Text score_text = get_text(score_font_size, Color::White, score_pos);
-	Text start_text = get_text(start_font_size, Color::Red, start_text_pos);
+	Text end_text = get_text(end_game_font_size, Color::Red);
+	Text score_text = get_text(score_font_size, Color::White);
+	Text start_text = get_text(start_font_size, Color::Red);
 	bool game_start = 0;
-
-	void end_screen();
 
 	void keep_mouse_in_bound();
 
-	void start_screen();
-
 	void draw_score();
+
+	void draw_text(Text& text, const std::string msg, Vector2f pos) const;
 
 public:
 	Game_engine(RenderWindow& game_wn, Font font, Player& player, EnemyManager& e_manager) : game_wn(game_wn), font(font), player(player), e_manager(e_manager) {}
--- 1st SFML Game/src/game_engine.cpp
+++ 1st SFML Game/src/game_engine.cpp
@@ -1,20 +1,31 @@
 #include "game_engine.h"
+#include <iostream>
 
-Text get_text(const int font_size, Color color, Vector2f pos)
+Text get_text(const int font_size, Color color)
 {
     Text end_text;
     end_text.setCharacterSize(font_size);
     end_text.setFillColor(color);
     end_text.setStyle(Text::Bold);
-    end_text.setOrigin(Vector2f(font_size / 2.f, font_size / 2.f));
-    end_text.setPosition(pos);
 
     return end_text;
 }
 
+void Game_engine::draw_text(Text& text, const std::string msg, Vector2f pos) const
+{
+    text.setFont(this->font);
+    text.setString(msg);
+
+    FloatRect bounds = text.getLocalBounds();
+    text.setOrigin(Vector2f(bounds.width / 2, bounds.height / 2));
+    text.setPosition(pos);
+
+    this->game_wn.draw(text);
+}
+
 void Game_engine::run()
 {
-    if (!this->game_start) { this->start_screen(); }
+    if (!this->game_start) { this->draw_text(start_text, "GAME!", start_text_pos); }
 
     else {
         this->keep_mouse_in_bound();
@@ -27,16 +38,8 @@ void Game_engine::run()
             e_manager.update(this->game_wn, this->game_timer.getElapsedTime());
 
         }
-        else { end_screen(); }
+        else { this->draw_text(end_text, "GAME OVER!", end_text_spawn); }
     }
-}
-
-void Game_engine::end_screen()
-{
-    this->end_text.setString("GAME OVER!");
-    this->end_text.setFont(this->font);
-
-    this->game_wn.draw(this->end_text);
 }
 
 void Game_engine::keep_mouse_in_bound()
@@ -48,20 +51,9 @@ void Game_engine::keep_mouse_in_bound()
     Mouse::setPosition(Vector2i(x_pos, y_pos), this->game_wn);
 }
 
-void Game_engine::start_screen()
-{
-    this->start_text.setString("GAME!");
-    this->start_text.setFont(this->font);
-
-    this->game_wn.draw(this->start_text);
-}
-
 
 void Game_engine::draw_score()
 {
     std::string score = "SCORE: " + std::to_string(this->e_manager.get_enemies_killed() * 10);
-    this->score_text.setFont(this->font);
-    this->score_text.setString(score);
-
-    this->game_wn.draw(this->score_text);
+    this->draw_text(score_text, score, score_pos);
 }
